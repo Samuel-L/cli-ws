c6564a569ef5bda092f61c294cef0740702dcb3b
Samuel <samuell.person@gmail.com>
2017-08-28 22:42:28 +0200

    Expanded on help flag

--- web_scraper/cli.py
+++ web_scraper/cli.py
@@ -15,13 +15,32 @@ def _verbose_echo(echo_string, verbose):
 @click.argument('url')
 @click.argument('target')
 @click.argument('target_type')
-@click.option('--location', default='./')
-@click.option('--filename', default='data')
-@click.option('--no-prettify', default=False, is_flag=True)
-@click.option('--dont-save', default=False, is_flag=True)
-@click.option('--verbose', default=False, is_flag=True)
+@click.option('--location', default='./', help='The location that you want the data file to be saved to')
+@click.option('--filename', default='data', help='The filename you want the data file to have')
+@click.option('--no-prettify', default=False, is_flag=True, help="Use this flag if you don't want prettified data")
+@click.option('--dont-save', default=False, is_flag=True, help="This won't save the data to a file, just output it")
+@click.option('--verbose', default=False, is_flag=True, help="Show verbose messages")
 def main(url, target, target_type, location, filename, no_prettify, dont_save, verbose):
-    """Entry Point"""
+    """A command line web scraper
+
+
+        Arguments:
+
+            url: the website you want to scrape for data
+
+            target: tag, class or id that you want to scrape
+
+            target_type: the type of target you want to scrape
+
+
+        Examples:
+
+            cli-ws example.com p tag
+
+            cli-ws example.com class_name class
+
+            cli-ws example.com id_name id
+    """
     html_document = str(core.fetch_html_document(url))
     _verbose_echo(f'Html document fetched from {url}.', verbose)
     
