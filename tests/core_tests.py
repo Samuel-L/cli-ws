76ff5896a0332c5163441d109681997fac43ec01
Samuel <samuell.person@gmail.com>
2017-08-28 21:05:04 +0200

    Add save_data_to_file function and tests

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -78,5 +78,26 @@ class TestScrapeTargetElementsMethod(unittest.TestCase):
         )
         self.assertEqual(test_elements.tag_specific_id_elements, str(scrape_target_elements))
 
+
+class TestSaveDataToFile(unittest.TestCase):
+    """Test save_data_to_file()"""
+    def setUp(self):
+        self.data = ['item1', 'item2', 'item3']
+        filename = 'dataFile'
+        self.created_file_location = core.save_data_to_file(self.data, filename=filename)
+    def test_creates_file(self):
+        """method should have created a file and returned its location"""
+        file_exists = os.path.isfile(self.created_file_location)
+        self.assertTrue(file_exists)
+    def test_saved_data_is_correct(self):
+        with open(self.created_file_location, 'r') as created_file:
+            written_data = created_file.readlines()
+        written_data = [item.rstrip('\n') for item in written_data]
+        self.assertEqual(self.data, written_data)
+
+    def tearDown(self):
+        """remove the created data file"""
+        os.remove(self.created_file_location)
+
 if __name__ == '__main__':
     unittest.main()
--- web_scraper/core.py
+++ web_scraper/core.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import random
 import requests
 from bs4 import BeautifulSoup
 
@@ -56,6 +57,25 @@ def prettify_scraped_data(scraped_data):
     return data_list
 
 
+def save_data_to_file(data, filename='data', location='./'):
+    """Return true if data has been saved to file
+
+    :param list data: a list that contains data
+    :param str filename: the desired filename
+    :param str location: the desired location for the data file
+    :return: file location
+    :rtype: str
+    """
+    filename = f'{filename}_{random.randint(10000, 50000)}.txt'
+    while os.path.isfile(filename):
+        filename = f'{filename}_{random.randint(10000, 50000)}.txt'
+    
+    with open(f'{location}\\{filename}', 'w') as data_file:
+        for item in data:
+            data_file.write(f'{item}\n')
+    return f'{location}\\{filename}'
+
+
 if __name__ == '__main__':
-    pass
+    pass 
 
