07e6c922b3bf0f749dc39fcd668dd8614cb1771d
Samuel <samuell.person@gmail.com>
2017-08-28 21:59:04 +0200

    Added colored output and verbose flag

--- web_scraper/cli.py
+++ web_scraper/cli.py
@@ -6,6 +6,11 @@ import click
 
 from web_scraper import core
 
+def _verbose_echo(echo_string, verbose):
+    if verbose:
+        click.secho(echo_string, fg='green')
+
+
 @click.command()
 @click.argument('url')
 @click.argument('target')
@@ -14,18 +19,22 @@ from web_scraper import core
 @click.option('--filename', default='data')
 @click.option('--no-prettify', default=False, is_flag=True)
 @click.option('--dont-save', default=False, is_flag=True)
-def main(url, target, target_type, location, filename, dont_save, no_prettify):
+@click.option('--verbose', default=False, is_flag=True)
+def main(url, target, target_type, location, filename, no_prettify, dont_save, verbose):
     """Entry Point"""
     html_document = str(core.fetch_html_document(url))
-
+    _verbose_echo(f'Html document fetched from {url}.', verbose)
+    
     scraped_data = core.scrape_target_elements(
         html_document,
         target=target,
         target_type=target_type
     )
+    _verbose_echo(f'Scraped all "{target}" targets with type "{target_type}" from the document.', verbose)
 
     if not no_prettify:
         scraped_data = core.prettify_scraped_data(scraped_data)
+        _verbose_echo(f'The scraped data has been prettified', verbose)
 
     if dont_save:
         for item in scraped_data:
@@ -36,7 +45,7 @@ def main(url, target, target_type, location, filename, dont_save, no_prettify):
             filename=filename,
             location=location
         )
-        click.echo(f'Data saved to {created_file_location}!')
+        click.secho(f'Data saved to {created_file_location}!', fg='green')
 
 
 if __name__ == '__main__':
