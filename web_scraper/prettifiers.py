2cc211207db198cf687ff4b93879fff4ebf07230
Samuel L <samuell.person@gmail.com>
2017-09-17 02:59:41 +0200

    Add regex prettifier

--- web_scraper/prettifiers.py
+++ web_scraper/prettifiers.py
@@ -2,6 +2,7 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import re
 
 def simple_prettifier(scraped_data):
     """Return more presentable data (in a list) provided by scrape_target_elements()
@@ -14,3 +15,19 @@ def simple_prettifier(scraped_data):
     for data in scraped_data:
         data_list.append(data.text)
     return data_list
+
+
+def regex_prettifier(scraped_data, regex):
+    """Return regex modified data (in a list).
+
+    :param list scraped_data: all the scraped data
+    :param str regex: the regular expression you want to use to prettify the
+    data
+    :return: list of regex modified data
+    :rtype: list
+    """
+    data_list = []
+    for data in scraped_data:
+        data_list.append(re.sub(regex, '', data))
+    return data_list
+
