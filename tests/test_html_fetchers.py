cdf9571792b09fd2ba31807d685f946e4aefb021
Samuel L <samuell.person@gmail.com>
2017-09-24 16:07:32 +0200

    Change to class and function names

--- tests/test_html_fetchers.py
+++ tests/test_html_fetchers.py
@@ -27,9 +27,9 @@ def mocked_requests_get(*args, **kwargs):
 	return MockResponse(404, (404, 'Not Found'))
 
 
-class TestHtmlFetchersMethods(unittest.TestCase):
+class TestFetchHtmlDocument(unittest.TestCase):
 	@mock.patch('web_scraper.core.html_fetchers.requests.get', side_effect=mocked_requests_get)
-	def test_fetch_html_document_200(self, mock_get):
+	def test_returns_200_and_html(self, mock_get):
 		"""fetch_html_document should return 200 and html"""
 		response = html_fetchers.fetch_html_document('http://example.com/') # reponse = tuple, MockResponse object
 		status_code = response[0][0]
@@ -37,7 +37,7 @@ class TestHtmlFetchersMethods(unittest.TestCase):
 
 		self.assertEqual((status_code, html), (200, 'html'))
 	@mock.patch('web_scraper.core.html_fetchers.requests.get', side_effect=mocked_requests_get)
-	def test_fetch_html_document_404(self, mock_get):
+	def test_returns_404_and_not_found(self, mock_get):
 		"""fetch_html_document should return 404 and 'Not Found'"""
 		response = html_fetchers.fetch_html_document('http://example.com/nonexistantpath') # reponse = tuple, MockResponse object.
 		status_code = response[0][0]
