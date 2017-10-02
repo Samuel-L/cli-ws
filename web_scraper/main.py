eeb7365151d602bf86bb53fae470bf787bdec864
Samuel L <samuell.person@gmail.com>
2017-10-02 21:25:42 +0200

    Moved logger setup

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -7,6 +7,10 @@ from web_scraper.cli import cli_parser, scraper
 
 
 def main(args):
+	logging.basicConfig(level=args.loglevel or logging.INFO, format='%(message)s')
+
+	logging.debug(f'Starting the script {sys.argv[0]}...')
+
 	if args.command == 'scrape':
 		scraper.scrape(args.url, args.target, args.target_type,
 			args.tag, args.exp, args.agent, args.name, args.path,
@@ -16,12 +20,5 @@ def main(args):
 
 if __name__ == '__main__':
 	args = cli_parser.parse_options()
-	if args.loglevel:
-		logging.basicConfig(level=args.loglevel, format='%(message)s')
-	else:
-		logging.basicConfig(level=logging.INFO, format='%(message)s')
-	
-	logging.debug(f'Starting the script {sys.argv[0]}...')
-
 	main(args)
 	
