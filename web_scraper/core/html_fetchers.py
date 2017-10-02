1ba12783fca76247447d84013d91f5c3073386a4
Samuel L <samuell.person@gmail.com>
2017-10-02 18:09:02 +0200

    Add user-agent field to html_fetcher

--- web_scraper/core/html_fetchers.py
+++ web_scraper/core/html_fetchers.py
@@ -5,17 +5,18 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import requests
 
 
-def fetch_html_document(url):
+def fetch_html_document(url, user_agent='python_requests.cli-ws'):
     """Fetch html from url and return html
 
     :param str url: an address to a resource on the Internet
+    :opt param str user_agent: user agent that the request will be made with
     :return no except hit: status code and html of page (if exists)
     :rtype: tuple
     :return except hit: error
     :rtype: str
     """
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers={'User-Agent': user_agent})
         if response.status_code == requests.codes.ok:
             return response.status_code, response.text # html
         else:
