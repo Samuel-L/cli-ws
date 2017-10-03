a388bcdcdf7f85c6e93c411b3459a5f49d5b4dbe
Samuel L <samuell.person@gmail.com>
2017-10-03 19:45:06 +0200

    Structure refactor

---
+++ web_scraper/cli/task.py
@@ -0,0 +1,76 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import logging
+from web_scraper.core import task_handlers
+from web_scraper.cli import helpers
+
+
+def create(args):
+	logging.debug(f'Creating a task for you...')
+	task_handlers.create_task(args.name, args.url, args.target, args.target_type,
+		task_group=args.group, tag=args.tag, keep_tags=args.keep_tags,
+		regex=args.exp, user_agent=args.agent, filename=args.fname,
+		path=args.path, dont_save=args.dont_save
+	)
+
+
+def remove(args):
+	logging.debug(f'Removing task {args.task}...')
+	task_handlers.remove_task(args.task)
+
+
+def show(args):
+	if args.task == 'all':
+		tasks = task_handlers.return_task(all_tasks=True)
+		if tasks:
+			logging.debug(f'Running task(s) for you...')
+			for task in tasks:
+				helpers.log_task(task)
+		else:
+			logging.warn('No tasks exists!')
+
+	elif args.task == 'group':
+		tasks = task_handlers.return_task(task_group=args.name)
+		if tasks:
+			logging.debug(f'Running task(s) for you...')
+			for task in tasks:
+				helpers.log_task(task)
+		else:
+			logging.warn(f'No such group ({args.name}) exists!')
+	elif args.task == 'name':
+		task = task_handlers.return_task(task_name=args.name)
+		if task:
+			helpers.log_task(task)
+		else:
+			logging.warn(f'That task ({args.name}) does not exist!')
+
+
+def run(args):
+	if args.task == 'all':
+		tasks = task_handlers.return_task(all_tasks=True)
+		if tasks:
+			logging.debug(f'Running task(s) for you...')
+			for task in tasks:
+				logging.debug('Current task: {}'.format(task['task_name']))
+				helpers.run_task(task)
+		else:
+			logging.warn('No tasks exists!')
+
+	elif args.task == 'group':
+		tasks = task_handlers.return_task(task_group=args.name)
+		if tasks:
+			logging.debug(f'Running task(s) for you...')
+			for task in tasks:
+				logging.debug('Current task: {}'.format(task['task_name']))
+				helpers.run_task(task)
+		else:
+			logging.warn(f'No such group ({args.name}) exists!')
+	elif args.task == 'name':
+		task = task_handlers.return_task(task_name=args.name)
+		if task:
+			logging.debug(f'Running task for you...')
+			helpers.run_task(task)
+		else:
+			logging.warn(f'That task ({args.name}) does not exist!')
--- web_scraper/main.py
+++ web_scraper/main.py
@@ -3,8 +3,7 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import logging
-import requests
-from web_scraper.cli import cli_parser, helpers
+from web_scraper.cli import cli_parser, scraper, task
 from web_scraper.core import task_handlers
 
 
@@ -14,76 +13,22 @@ def main(args):
 	logging.debug(f'Starting the script...')
 
 	if args.command == 'scrape':
-		logging.debug(f'Scraping data from {args.url}...')
-		helpers.scrape(args.url, args.target, args.target_type,
+		scraper.scrape(args.url, args.target, args.target_type,
 			args.tag, args.exp, args.agent, args.fname, args.path,
 			args.dont_save, args.keep_tags
 		)
 	
 	elif args.command == 'create_task':
-		logging.debug(f'Creating a task for you...')
-		task_handlers.create_task(args.name, args.url, args.target, args.target_type,
-			task_group=args.group, tag=args.tag, keep_tags=args.keep_tags,
-			regex=args.exp, user_agent=args.agent, filename=args.fname,
-			path=args.path, dont_save=args.dont_save
-		)
+		task.create(args)
 
 	elif args.command == 'remove_task':
-		logging.debug(f'Removing task {args.task}...')
-		task_handlers.remove_task(args.task)
+		task.remove(args)
 
 	elif args.command == 'show_task':
-		if args.task == 'all':
-			tasks = task_handlers.return_task(all_tasks=True)
-			if tasks:
-				logging.debug(f'Running task(s) for you...')
-				for task in tasks:
-					helpers.log_task(task)
-			else:
-				logging.warn('No tasks exists!')
-
-		elif args.task == 'group':
-			tasks = task_handlers.return_task(task_group=args.name)
-			if tasks:
-				logging.debug(f'Running task(s) for you...')
-				for task in tasks:
-					helpers.log_task(task)
-			else:
-				logging.warn(f'No such group ({args.name}) exists!')
-		elif args.task == 'name':
-			task = task_handlers.return_task(task_name=args.name)
-			if task:
-				helpers.log_task(task)
-			else:
-				logging.warn(f'That task ({args.name}) does not exist!')
+		task.show(args)
 	
 	elif args.command == 'run_task':
-		if args.task == 'all':
-			tasks = task_handlers.return_task(all_tasks=True)
-			if tasks:
-				logging.debug(f'Running task(s) for you...')
-				for task in tasks:
-					logging.debug('Current task: {}'.format(task['task_name']))
-					helpers.run_task(task)
-			else:
-				logging.warn('No tasks exists!')
-
-		elif args.task == 'group':
-			tasks = task_handlers.return_task(task_group=args.name)
-			if tasks:
-				logging.debug(f'Running task(s) for you...')
-				for task in tasks:
-					logging.debug('Current task: {}'.format(task['task_name']))
-					helpers.run_task(task)
-			else:
-				logging.warn(f'No such group ({args.name}) exists!')
-		elif args.task == 'name':
-			task = task_handlers.return_task(task_name=args.name)
-			if task:
-				logging.debug(f'Running task for you...')
-				helpers.run_task(task)
-			else:
-				logging.warn(f'That task ({args.name}) does not exist!')
+		task.run(args)
 
 
 if __name__ == '__main__':
