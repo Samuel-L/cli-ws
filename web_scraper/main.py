fc8546354771a396dd181aa0b0abd0af5e759a54
Samuel <samuell.person@gmail.com>
2017-10-08 20:45:53 +0200

    Add if statement that ensures cli_ws folder exists in Documents

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import logging
 from web_scraper.cli import cli_parser, scraper, task
-from web_scraper.core import task_handlers
+from web_scraper.core import task_handlers, file_handlers
 
 
 def main(args):
@@ -17,7 +17,7 @@ def main(args):
 			args.tag, args.exp, args.agent, args.fname, args.path,
 			args.dont_save, args.keep_tags
 		)
-	
+
 	elif args.command == 'create_task':
 		task.create(args)
 
@@ -26,11 +26,15 @@ def main(args):
 
 	elif args.command == 'show_task':
 		task.show(args)
-	
+
 	elif args.command == 'run_task':
 		task.run(args)
 
 
 if __name__ == '__main__':
+	documents_folder_path = os.path.expanduser('~/Documents/cli_ws_1.0.0')
+	if not os.path.isdir(documents_folder_path):
+		create_documents_folder()
+
 	args = cli_parser.parse_options()
 	main(args)
